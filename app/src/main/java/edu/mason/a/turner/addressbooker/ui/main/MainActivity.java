eb81a4e48840109c84ad2af130a4885d6107f1a7
Mason Turner <masonaveryt@gmail.com>
2021-11-30 02:43:52 -0700

    optimized imports

--- app/src/main/java/edu/mason/a/turner/addressbooker/ui/main/MainActivity.java
+++ app/src/main/java/edu/mason/a/turner/addressbooker/ui/main/MainActivity.java
@@ -4,17 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import edu.mason.a.turner.addressbooker.R;
 import edu.mason.a.turner.addressbooker.UpdateContactActivity;
 import edu.mason.a.turner.addressbooker.data.Contact;
 import edu.mason.a.turner.addressbooker.data.ContactDatabase;
